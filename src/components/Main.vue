62c8e5f9f066a7d6ecee0fcf0ca64773ed897ca2
Ghadeer <ghaora@gmail.com>
2022-02-02 17:22:10 +0200

    first commit axios

---
+++ src/components/Main.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+      <SearchBar/>
+    
+  </main>
+</template>
+
+<script>
+import axios from "axios";
+import SearchBar from "./commons/SearchBar.vue"
+
+export default {
+  name: 'Main',
+  components: {
+      SearchBar 
+  },
+  methods: {
+      getInfo: function(){
+        axios.get('https://api.themoviedb.org/3/movie', {
+        params: {
+            api_key: '5f763b82935ccf722598ab22ec49212e',
+            query: 'Potter'
+        }
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+     
+      }
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/commons/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <form action="/action_page.php">
+        <input type="text" id="fname" name="fname">
+        <button>Submit</button>
+    </form>
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'SearchBar',
+  components: {
+     
+  },
+ 
+}
+</script>
+
+<style lang="scss">
+
+</style>
